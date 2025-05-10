ad21d20e24c65208c7409cba0ab6539b0e23512f
Mohammad Anzer <U1969730@unimail.hud.ac.uk>
2025-05-10 02:43:55 +0100

    Projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Projects from "./components/Projects";
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
       <About />
+      <Projects />
     </>
   );
 }
