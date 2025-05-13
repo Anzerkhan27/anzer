5f1644702312536ca8f9ac72c866683ddaa3df66
Mohammad Anzer <U1969730@unimail.hud.ac.uk>
2025-05-13 11:05:32 +0100

    Admin page

--- backend/portfolio/models.py
+++ backend/portfolio/models.py
@@ -1,13 +1,11 @@
 from django.db import models
 
-# Create your models here.
-
-
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    link = models.URLField(blank=True)
-    image = models.ImageField(upload_to='projects/', blank=True)
+    github_link = models.URLField(blank=True)
+    live_link = models.URLField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
