fe2b60b82f6b4e3f4ba336d7f1200f01b990c231
Mohammad Anzer <U1969730@unimail.hud.ac.uk>
2025-05-13 11:28:03 +0100

    Restframework installed correctly, project serializer tested

---
+++ backend/portfolio/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import ProjectListAPIView
+
+urlpatterns = [
+    path('projects/', ProjectListAPIView.as_view(), name='project-list'),
+]
+
+
--- backend/portfolio/views.py
+++ backend/portfolio/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
 # Create your views here.
+from rest_framework import generics
+from .models import Project
+from .serializers import ProjectSerializer
+
+class ProjectListAPIView(generics.ListAPIView):
+    queryset = Project.objects.all().order_by('-created_at')
+    serializer_class = ProjectSerializer
+
+
